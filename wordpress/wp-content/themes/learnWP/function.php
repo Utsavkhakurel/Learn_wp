87de20a026b0e042e497601220821654c4c94701
Utsav khakurel <utsavkhak22@gmail.com>
2025-12-18 07:32:10 +0545

    wordpress

--- wordpress/wp-content/themes/learnWP/function.php
+++ wordpress/wp-content/themes/learnWP/function.php
@@ -1,9 +1,7 @@
 <?php
-function learn_wp_enqueue_styles() {
-wp_enqueue_style ( 'tailwindcss', get_template_directory_uri() . '/src/output.css', array(), '1.0', 'all' );
-wp_enqueue_style ( 'learnWP-style', get_stylesheet(), [], '1.0.1', 'all' );
-
-
+function learn_wp_enqueue_styles()
+{
+    wp_enqueue_style('tailwindcss', get_template_directory_uri() . '/src/output.css', array(), '1.0', 'all');
+    wp_enqueue_style('learnWP-style', get_stylesheet(), [], '1.0.1', 'all');
 }
-add_action ('wp_enqueue_scripts', 'learn_wp_styles' );
-?>
+add_action('wp_enqueue_scripts', 'learn_wp_styles');
--- wordpress/wp-content/themes/learnWP/header.php
+++ wordpress/wp-content/themes/learnWP/header.php
@@ -1,9 +1,11 @@
 <?php get_header() ?>
 <!DOCTYPE html>
 
-<html class="light" lang="en"><head>
-<meta charset="utf-8"/>
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<title>Olivia Chen - Digital Product Designer</title>
-<?php wp_head(); ?>
-</body></html>
+<html class="light" lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Olivia Chen - Digital Product Designer</title>
+    <?php wp_head(); ?>
+    </body>
